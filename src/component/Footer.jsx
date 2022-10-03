a3bffed0e91f0b78214bb805d8f257bdb801c37b
kavitha <tamilkavitha56@gmail.com>
2022-10-02 20:33:41 -0700

    first commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+function Footer(){
+    return(
+        <>
+        <div className="container-fluid">
+        <div className="card text-center">
+  <div className="card-header">
+    Featured
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Special title treatment</h5>
+    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    
+  </div>
+  <div className="card-footer text-muted">
+    All rights reserved
+  </div>
+</div>
+
+        </div>
+        </>
+    )
+}
+export default Footer;
---
+++ src/component/Main.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+function Main(){
+    return(
+        <>
+        <div className="jumbotron jumbotron-fluid text-center">
+            <div className='container'>
+  <h1 className="display-4">Welcome Everyone</h1>
+  <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+ 
+  </div>
+</div>
+        <div className='container my-5'>
+            <div className="row">
+            <div className='col-3'>
+            <div className="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyx2HIw5F6Yw-a6-5k6Qo9NM1A8GsXOCapnQ&usqp=CAU" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Title 1</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-dark">Know More</a>
+  </div>
+</div>
+</div>
+<div className='col-3'>
+            <div className="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1ZZM_In3TLIfZ4wT9yWZrummKDu5_3T9C-g&usqp=CAU" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Title 2</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-dark">Know More</a>
+  </div>
+</div>
+</div>
+<div className='col-3'>
+            <div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgoNZX9JmCtG0AIoLnvMmwzfx49_xJxlvyDg&usqp=CAU" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Title 3</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-dark">Know More</a>
+  </div>
+</div>
+</div>
+<div className='col-3'>
+            <div className="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM51kzaEiUgxzAsUvpuqcdFOUEt7Yr7OqS1Q&usqp=CAU" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Title 4</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-dark">Know More</a>
+  </div>
+</div>
+</div>
+</div>
+</div>
+       
+        </>
+    )
+}
+export default Main;
